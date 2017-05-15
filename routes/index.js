6901fb45651230e59d73da6d685e77243a7ff928
Kevin <Kevin@Kevin.local>
2017-05-16 00:36:16 +0800

    log4js

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Kevin422' });
 });
 
 module.exports = router;
